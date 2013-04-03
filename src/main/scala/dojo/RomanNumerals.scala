69442bc0dc210395eab66d5caf452683011dee61
Alexander Daniel <alexander.daniel@gmx.at>
2013-04-03 20:00:48 +0200

    coding dojo

--- src/main/scala/dojo/RomanNumerals.scala
+++ src/main/scala/dojo/RomanNumerals.scala
@@ -2,19 +2,19 @@ package dojo
 
 object RomanNumerals {
 
-
+  val c = Map(0 -> "I", 1 -> "V", 2 -> "X")
 
   def roman(number: Int): String = {
     number match {
-      case n if n <= 3 => "I" * n
-      case 4 => "IV"
-      case 9 => "IX"
-      case 10 => "X"
-      case n  if n > 10  => "XI"
+      case n if n <= 3 => c(0) * n
+      case 4 => c(0) + c(1)
+      case 9 => c(0) + c(2)
+      case 10 => c(2)
+      case n  if n > 10  => c(2) + c(0)
 
 
-      case 11 => "XI"
-      case n if n >= 5 => "V" + "I" * (n-5)
+      case 11 => c(2) + c(0)
+      case n if n >= 5 => c(1) + c(0) * (n-5)
       case _ => throw new RuntimeException
     }
   }
