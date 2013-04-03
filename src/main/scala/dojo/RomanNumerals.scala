d2e7c7526ec44e4f6634692d15de051375de6344
Alexander Daniel <alexander.daniel@gmx.at>
2013-04-03 20:15:46 +0200

    dojo

--- src/main/scala/dojo/RomanNumerals.scala
+++ src/main/scala/dojo/RomanNumerals.scala
@@ -3,12 +3,13 @@ package dojo
 object RomanNumerals {
 
   val c = List(
-    Map(0 -> "I", 1 -> "V", 2 -> "X")
+    Map(0 -> "I", 1 -> "V", 2 -> "X"),
+    Map(0 -> "X", 1 -> "L", 2 -> "C")
   )
 
 
   def roman(number: Int): String = {
-   roman1(number, 0)
+    roman1(number/10, 1) + roman1(number%10, 0)
   }
   def roman1(number: Int, pos: Int) = {
     number match {
@@ -19,7 +20,6 @@ object RomanNumerals {
       case n  if n > 10  => c(pos)(2) + c(pos)(0)
 
 
-      case 11 => c(pos)(2) + c(pos)(0)
       case n if n >= 5 => c(pos)(1) + c(pos)(0) * (n-5)
       case _ => throw new RuntimeException
     }
